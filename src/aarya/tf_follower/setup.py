cc5639f1fc9636d0eb57910341e7e93161d1ce99
Aarya <ak-258066@hs-weingarten.de>
2026-07-20 00:42:35 +0200

    Task 4 done-ish task4final and normal node+costmap clearer. Could add collision avoider if reqd.

--- src/aarya/tf_follower/setup.py
+++ src/aarya/tf_follower/setup.py
@@ -25,6 +25,7 @@ setup(
             'tf_follower_node_v2pt2 = tf_follower.tf_follower_v2pt2:main',
             "tf_follower_task3 = tf_follower.tf_follower_task3:main",
             "tf_follower_task3v2 = tf_follower.tf_follower_task3v2:main",
+            "costmap_clearer = tf_follower.costmap_clearer:main",
         ],
     },
 )
